0e2960234639d6d37e61c168c80a7f0e37b4393c
Typical Engineer <milanthegamer@gmail.com>
2021-09-25 10:52:54 +0530

    Create Block and Get Previous Block functions added

--- dist/services/blockchain.service.js
+++ dist/services/blockchain.service.js
@@ -1,7 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var Blockchain = /** @class */ (function () {
+    //Constructor
     function Blockchain() {
+        var _this = this;
+        //Create Block
+        this.createBlock = function (proof, previousHash) {
+            return new Promise(function (resolve, reject) {
+                try {
+                    var block = {
+                        index: _this.chain.length + 1,
+                        timestamp: String(new Date()),
+                        proof: proof,
+                        previousHash: previousHash,
+                        data: {},
+                    };
+                    _this.chain.push(block);
+                    resolve(block);
+                }
+                catch (err) {
+                    reject(err);
+                }
+            });
+        };
         this.chain = [];
     }
     return Blockchain;
--- src/services/blockchain.service.ts
+++ src/services/blockchain.service.ts
@@ -1,9 +1,50 @@
+//Interfaces
+interface Block {
+    index: Number;
+    timestamp: String;
+    proof: any;
+    previousHash: String;
+    data: Object;
+}
 class Blockchain {
-    chain: [];
+    //Define Class variables
+    chain: Block[];
 
+    //Constructor
     constructor() {
         this.chain = [];
     }
+
+    //Create Block
+    createBlock = (proof: any, previousHash: String): Promise<Block> => {
+        return new Promise((resolve, reject) => {
+            try {
+                let block: Block = {
+                    index: this.chain.length + 1,
+                    timestamp: String(new Date()),
+                    proof: proof,
+                    previousHash: previousHash,
+                    data: {},
+                };
+                this.chain.push(block);
+                resolve(block);
+            } catch (err) {
+                reject(err);
+            }
+        });
+    };
+
+    //Get Previcous Block
+    getPreviousBlock = (): Promise<Block> => {
+        return new Promise((resolve, reject) => {
+            try {
+                let lastBlock: Block = this.chain[-1];
+                resolve(lastBlock);
+            } catch (err) {
+                reject(err);
+            }
+        });
+    };
 }
 
 export default Blockchain;
